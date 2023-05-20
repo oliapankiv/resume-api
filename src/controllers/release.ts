ccfde41995b3ac0b3a655733ba007a1e3b3c3a56
Andrew Nyzhnyk <andrew@awkwrd.dev>
2023-05-20 23:01:17 +0200

    initital webhook

--- src/controllers/release.ts
+++ src/controllers/release.ts
@@ -1,16 +1,10 @@
-import { crypto } from 'https://deno.land/std@0.182.0/crypto/mod.ts'
 import { Middleware, Status } from 'https://deno.land/x/oak@v12.1.0/mod.ts'
 
-import { useResume } from '~/services/mod.ts'
-
-const secret = Deno.env.get('WEBHOOK')
-if (!secret) throw new Error('no secret defined')
-
-const encodr = new TextEncoder()
-const algorithm: HmacImportParams = { name: 'hmac', hash: 'sha-256' }
-const key = await crypto.subtle.importKey('raw', encodr.encode(secret), algorithm, false, ['sign']);
+import { useResume, useWebhook } from '~/services/mod.ts'
 
 const validator: Middleware = async (ctx, next) => {
+  const { verify } = useWebhook()
+
   const payload = await ctx.request.body({ type: 'json', limit: 64 * 1024 }).value
   if (!payload) return ctx.throw(Status.BadRequest, 'missing payload')
 
@@ -19,10 +13,9 @@ const validator: Middleware = async (ctx, next) => {
   const signature = ctx.request.headers.get('x-hub-signature-256')
   if (!signature) return ctx.throw(Status.BadRequest, 'missing signature')
 
-  const checksum = await crypto.subtle.sign(algorithm.name, key, encodr.encode(JSON.stringify(payload)))
-  const hex = [...new Uint8Array(checksum)].map((byte) => byte.toString(16).padStart(2, '0')).join('')
+  const isValid = await verify(signature, payload)
 
-  return signature.slice(7) === hex ? next() : ctx.throw(Status.Unauthorized, 'signature mismatch')
+  return isValid ? next() : ctx.throw(Status.Unauthorized, 'signature mismatch')
 }
 
 const handler: Middleware = async (ctx) => {
--- src/services/useWebhook.ts
+++ src/services/useWebhook.ts
@@ -1,3 +1,17 @@
-export const useWebhook = () => {
+const secret = Deno.env.get('WEBHOOK')
+if (!secret) throw new Error('no secret defined')
 
+const encodr = new TextEncoder()
+const algorithm: HmacImportParams = { name: 'hmac', hash: 'sha-256' }
+const key = await crypto.subtle.importKey('raw', encodr.encode(secret), algorithm, false, ['sign']);
+
+export const useWebhook = () => {
+  const verify = async (signature: string, payload: unknown): Promise<boolean> => {
+    const checksum = await crypto.subtle.sign(algorithm.name, key, encodr.encode(JSON.stringify(payload)))
+    const hex = [...new Uint8Array(checksum)].map((byte) => byte.toString(16).padStart(2, '0')).join('')
+
+    return signature === hex
+  }
+
+  return { verify }
 }
