d955d8e123ed0c3845d4f41cd002fbabec1d0ccf
Andrew Nyzhnyk <andrew@nyzhnyk.com>
2024-05-07 09:30:25 +0200

    added: favicon content-type

--- src/controllers/favicon.ts
+++ src/controllers/favicon.ts
@@ -1,5 +1,6 @@
 import { Middleware } from 'oak'
 
+const type = 'image/vnd.microsoft.icon'
 const body = Deno.readFileSync('./src/assets/favicon.ico')
 
-export const favicon: Middleware = ({ response }) => (response.body = body);
+export const favicon: Middleware = ({ response }) => Object.assign(response, { body, type });
